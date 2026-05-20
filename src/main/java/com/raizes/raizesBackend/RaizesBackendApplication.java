f69196218e0ef024e70e6bec7bf2741a06cf011b
Thiago Bellini <thiagobellini52@gmail.com>
2026-05-20 11:29:16 -0300

    Projeto criado com todas as dependências

---
+++ src/main/java/com/raizes/raizesBackend/RaizesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.raizes.raizesBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaizesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaizesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raizes/raizesBackend/RaizesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raizes.raizesBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaizesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
