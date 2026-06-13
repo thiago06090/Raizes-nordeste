4d9d61d13843bef532e4f9d859110656b6ecdaf0
Thiago Bellini <thiagobellini52@gmail.com>
2026-06-13 11:34:52 -0300

    adiciona estoque inicial para unidades 2 e 3

---
+++ src/main/resources/db/migration/V10__seed_estoque_unidades_2_3.sql
@@ -0,0 +1,7 @@
+-- Estoque unidade 2 (Fortaleza)
+INSERT INTO estoque (unidade_id, produto_id, quantidade) VALUES
+                                                             (2, 1, 40), (2, 2, 25), (2, 3, 15), (2, 4, 35), (2, 5, 80);
+
+-- Estoque unidade 3 (Salvador)
+INSERT INTO estoque (unidade_id, produto_id, quantidade) VALUES
+                                                             (3, 1, 30), (3, 2, 20), (3, 3, 10), (3, 4, 25), (3, 5, 60);
